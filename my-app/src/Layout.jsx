03d0c3f4280127a301fa6550cc4e734148b00fb1
Jibru-ski <barakajibran2004@gmail.com>
2025-06-17 09:26:40 +0300

    Home page

--- my-app/src/Layout.jsx
+++ my-app/src/Layout.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
+import Logo from './assets/logo.png'
 
 const Layout = () => {
   return (
     <>
-    <header className="header">
-        <h1>My Application</h1>
+    <header className="blurry">
+        <div className="logo">
+          <img src={Logo} alt="Logo" height={60}/>
+        </div>
+        <nav>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/about"}>About</Link>
+          <Link to={"/contact"}>Contact</Link>
+        </nav>
+        <nav className="Login">
+          <Link to={"/"}>Login</Link>
+          <Link to={"/"}>Signup</Link>
+        </nav>
       </header>
-      <main className="main-content">
+      <main>
         <Outlet />
       </main>
       <footer className="footer">
-        <p>&copy; 2024 My Application</p>
+        <p>&copy; 2025 Citeberry</p>
       </footer>
     </>
   )
--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import '../App.css'
+import HeroImg from '../assets/home-hero-img.png'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <></>
+    <>
+    <div className="home-hero">
+        <div className="home-hero-text">
+            <h1>Quote <span>Smarter</span>, Close <span>Faster</span></h1>
+            <h4>Generate accurate, client-ready quotes in minutes — no spreadsheets, no hassle.</h4>
+            <Link className='hero-btn' to='/login'><button className='btn'>Get Started</button></Link>
+        </div>
+        <div className="hero-img">
+          <img src={HeroImg} alt="" height={300}/>
+        </div>
+    </div>
+    
+    
+    </>
   )
 }
 
